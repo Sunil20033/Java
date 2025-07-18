4cd31b9d35f66b4ab83dc017e053d8619e767fdc
Sunil20033 <suniljawargi@gmail.com>
2025-07-18 20:05:33 +0530

    Variabldes,DataTypes & String

---
+++ Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+  byte StudentGrade;
+  short StudenteID;
+  int StudentMarks;
+  long StudentPhone;
+  public static void main(String[] args) {
+    String StudentName = "Shashikumar";
+    String StudentName1 = "Shashi";
+    short StudentID = 100;
+    long StudentPhone = 7618781239l;
+    char Grade = 'A';
+    System.out.println(StudentName+StudentID+StudentPhone);
+    System.out.println("StudentName:"+StudentName);
+    System.out.println("StudentID:"+StudentID);
+    System.out.println("Grade:" + Grade);
+
+  }
+}
